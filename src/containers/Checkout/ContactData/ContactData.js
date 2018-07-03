45b19a0402c5967c405e53e3ad6ab38b9c2790da
chris <christian.bracher@gmail.com>
2018-07-03 16:58:04 +0800

    Implemented Form submission and Dynamic Form building through State. Working!

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -5,35 +5,92 @@ import { withRouter} from 'react-router-dom';
 import classes from './ContactData.css';
 import Button from '../../../components//UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
         loading: false,
 
-        name: '',
-        email: '',
-        street: '',
-        postcode: '',
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name',
+                    autoFocus: true,
+                },
+                value: '',
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your Email',
+                },
+                value: '',
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street',
+                },
+                value: '',
+            },
+            postcode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Postal Code',
+                },
+                value: '',
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country',
+                },
+                value: '',
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value: 'fastest', displayValue: 'Fastest'},
+                        {value: 'cheapest', displayValue: 'Cheapest'},
+                    ]
+                },
+                value: 'fastest',
+            },
+        }
     }
 
-    inputChangeHandler = (event) => {
-        this.setState({[event.target.name]: event.target.value});
+    inputChangeHandler = (event, inputId) => {
+        const updatedOrderForm = {...this.state.orderForm};
+        
+        const updatedFormElements = {...updatedOrderForm[inputId]};
+        
+        updatedFormElements.value = event.target.value;
+        updatedOrderForm[inputId] = updatedFormElements;
+
+        this.setState({orderForm: updatedOrderForm});
     }
 
     orderBtnHandler = (event) => {
         event.preventDefault();
         this.setState({loading: true});
+
+        const orderFormFields = {...this.state.orderForm};
+        let customer = {};
+        for (let field in orderFormFields) {
+            customer[field] = orderFormFields[field].value; 
+        }
+
         const order = {
             ingredients: this.props.ingredients,
             totalPrice: this.props.price,
-            customer: {
-                name: this.state.name,
-                email:this.state.email,
-                address: {
-                    street: this.state.street,
-                    postcode: this.state.postcode,
-                },
-            },
+            customer: customer,
         };
 
         axios.post('https://burger-builder-e8d73.firebaseio.com/orders.json', order)
@@ -49,38 +106,67 @@ class ContactData extends Component {
 
     render () {
 
+        const formElementArray = [];
+
+        for (let key in this.state.orderForm) {
+            formElementArray.push({
+                id: key,
+                config: this.state.orderForm[key],
+
+            })
+        }
+
         let form = (
             <form>
-                <input className={classes.Input} 
+                {formElementArray.map((formElement) => {
+                    return (
+                        <Input
+                            key={formElement.id}
+                            name={formElement.id}
+                            elementType={formElement.config.elementType}
+                            elementConfig={formElement.config.elementConfig}
+                            value={formElement.config.value}
+                            changed={(event)=>this.inputChangeHandler(event, formElement.id)}
+                        />
+                    );
+                })}
+
+                {/* <Input
+                    inputtype='input' 
                     type='text' 
                     name='name' 
                     placeholder='Your name'
                     autoFocus
                     value={this.state.name}
-                    onChange={this.inputChangeHandler}
+                    onChange={this.inputChangeHandler} 
                 />
+
                     
-                <input className={classes.Input} 
+                <Input
+                    inputtype='input' 
                     type='email' 
                     name='email' 
                     placeholder='Your email'
                     value={this.state.email}
                     onChange={this.inputChangeHandler}
                 />
-                <input className={classes.Input} 
+                <Input
+                    inputtype='input' 
                     type='text' 
                     name='street' 
                     placeholder='Street'
                     value={this.state.street}
                     onChange={this.inputChangeHandler}
                 />
-                <input className={classes.Input} 
+                <Input
+                    inputtype='input' 
                     type='text' 
                     name='postcode' 
                     placeholder='Postal Code'
                     value={this.state.postcode}
                     onChange={this.inputChangeHandler}
-                />
+                /> */}
+
                 <Button btnType='Success'
                     clicked={this.orderBtnHandler} 
                 >ORDER
